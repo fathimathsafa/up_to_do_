951f2b354c21f48386a0827fa99d675bf63c494c
fathimathsafa <safahaneefa01@gmail.com>
2024-05-14 15:57:23 +0530

    color constants created

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class ColorTheme {
   static Color maincolor = Color(0xfff9F55D5);
   static Color mainTextcolor = Colors.white;
+   static Color secondColor = Color(0xfffAFAFAF);
+      static Color secondTextColor = Colors.black;
+
 }
